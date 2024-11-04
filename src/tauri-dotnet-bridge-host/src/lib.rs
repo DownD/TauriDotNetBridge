d5b88f2b9d541ddb170eeb56396eac5051080e7b
plainionist <mail@plainionist.net>
2024-11-04 17:03:53 +0100

    host: converting strings fixed

--- src/tauri-dotnet-bridge-host/src/lib.rs
+++ src/tauri-dotnet-bridge-host/src/lib.rs
@@ -1,7 +1,6 @@
 use lazy_static::lazy_static;
-use netcorehost::{hostfxr::AssemblyDelegateLoader, nethost, pdcstr};
+use netcorehost::{hostfxr::AssemblyDelegateLoader, nethost, pdcstring::PdCString};
 use std::env;
-use std::ffi::{c_char, CString};
 
 lazy_static! {
     static ref ASM: AssemblyDelegateLoader = {
@@ -12,36 +11,32 @@ lazy_static! {
             .parent()
             .expect("Failed to get the executable directory")
             .join("dotnet");
-        
-        let runtime_config_path = dotnet_dir.join("TauriDotNetBridge.runtimeconfig.json");
-        let dll_path = dotnet_dir.join("TauriDotNetBridge.dll");
+
+        let runtime_config_path = PdCString::try_from(
+            dotnet_dir
+                .join("TauriDotNetBridge.runtimeconfig.json")
+                .to_str()
+                .expect("Failed to convert path to string"),
+        )
+        .expect("Failed to convert runtime config path to PdCString");
+
+        let dll_path = PdCString::try_from(
+            dotnet_dir
+                .join("TauriDotNetBridge.dll")
+                .to_str()
+                .expect("Failed to convert path to string"),
+        )
+        .expect("Failed to convert DLL path to PdCString");
 
         println!("Using TauriDotNetBridge.runtimeconfig.json: {:?}", runtime_config_path);
         println!("Using TauriDotNetBridge.dll: {:?}", dll_path);
-    
+
         let context = hostfxr
-            .initialize_for_runtime_config(runtime_config_path.as_path())
+            .initialize_for_runtime_config(&runtime_config_path)
             .expect("Invalid runtime configuration");
 
         context
-            .get_delegate_loader_for_assembly(dll_path.as_path())
+            .get_delegate_loader_for_assembly(dll_path)
             .expect("Failed to load DLL")
     };
-}
-
-pub fn process_request(request: &str) -> String {
-    let instance = &ASM;
-
-    let process_request = instance
-        .get_function_with_unmanaged_callers_only::<fn(text_ptr: *const u8, text_length: i32) -> *mut c_char>(
-            pdcstr!("TauriDotNetBridge.Bridge, TauriDotNetBridge"),
-            pdcstr!("ProcessRequest"),
-        )
-        .unwrap();
-
-    let response_ptr = process_request(request.as_ptr(), request.len() as i32);
-
-    let response = unsafe { CString::from_raw(response_ptr) };
-
-    format!("{}", response.to_string_lossy())
 }
